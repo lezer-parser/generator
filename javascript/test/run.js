5ee6f0807c99247ce04e1c3277b5f0412e6307bd
Marijn Haverbeke <marijn@haverbeke.nl>
2019-04-19 10:46:13 +0200

    Start adding tests for the JS grammar

---
+++ javascript/test/run.js
@@ -0,0 +1,58 @@
+const {Parser, StringStream} = require("lezer")
+const {buildParser} = require("../..")
+const ist = require("ist")
+
+let fs = require("fs"), path = require("path")
+let caseDir = __dirname
+
+function compressAST(ast, file) {
+  let token = /\s*($|[(),]|\"(?:\\.|[^"])*\"|[\w⚠]+)/gy
+  let result = ""
+  for (;;) {
+    let m = token.exec(ast)
+    if (!m) throw new Error("Invalid AST spec in " + file)
+    if (!m[1]) break
+    result += m[1]
+  }
+  return result
+}
+
+let parser = null
+let force = () => {
+  if (!parser) {
+    let text = fs.readFileSync(path.join(__dirname, "../src/javascript.grammar"), "utf8")
+    parser = buildParser(text, "javascript.grammar")
+  }
+  return parser
+}
+
+describe("JavaScript cases", () => {
+  for (let file of fs.readdirSync(caseDir)) {
+    if (!/\.txt$/.test(file)) continue
+    let name = /^[^\.]*/.exec(file)[0]
+    let content = fs.readFileSync(path.join(caseDir, file), "utf8")
+    let parts = content.split(/\n---+\n/)
+
+    for (let i = 0; i < parts.length; i++) it(`case ${name}:${i + 1}`, () => {
+      let [text, ast] = parts[i].split(/\n==+>/)
+      if (!ast) throw new Error(`Missing syntax tree in ${name}:${i + 1}`)
+      let expected = compressAST(ast, file)
+      let strict = expected.indexOf("⚠") < 0, parser = force()
+      let result = parser.parse(new StringStream(text.trim()), {strict})
+      let parsed = result.toString(parser)
+      if (parsed != expected) {
+        if (parsed.length > 76) {
+          let mis = 0
+          while (parsed[mis] == expected[mis]) mis++
+          if (mis > 30) {
+            parsed = "…" + parsed.slice(mis - 30)
+            expected = "…" + expected.slice(mis - 30)
+          }
+        }
+        if (parsed.length > 76) parsed = parsed.slice(0, 75) + "…"
+        if (expected.length > 76) expected = expected.slice(0, 75) + "…"
+        throw new Error(`Output mismatch, got\n  ${parsed}\nexpected\n  ${expected}`)
+      }
+    })
+  }
+})
