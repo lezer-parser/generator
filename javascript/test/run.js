3cbfbda0cbc3faadcf0b90455d1daa0f41404b84
Marijn Haverbeke <marijn@haverbeke.nl>
2019-05-15 12:04:13 +0200

    Start adjusting the JS grammar to changes in the generator

--- javascript/test/run.js
+++ javascript/test/run.js
@@ -18,26 +18,51 @@ function compressAST(ast, file) {
 }
 
 function externalTokenizer(name, terms) {
-  const newline = /[\n\u2028\u2029]/, brace = "}".charCodeAt(0)
+  const newline = /[\n\u2028\u2029]/, brace = "}".charCodeAt(0), semicolon = ";".charCodeAt(0)
   if (name == "insertSemicolon") {
     return new ExternalTokenizer((input, stack) => {
       let start = input.pos, next = input.next()
-      if (next == brace || next == -1 || newline.test(input.read(stack.pos, input.pos - 1)))
+      if ((next == brace || next == -1 || newline.test(input.read(stack.pos, input.pos - 1))) && stack.canShift(terms.insertSemi))
         input.accept(terms.insertSemi, start)
     })
-  } else if (name == "noInsertSemicolon") {
+  } else if (name == "noSemicolon") {
     return new ExternalTokenizer((input, stack) => {
       let start = input.pos, next = input.next()
-      if (next != brace && next != -1 && !newline.test(input.read(stack.pos, input.pos - 1)))
-        input.accept(terms.noInsertSemi, start)
+      if (next != brace && next != semicolon && next != -1 &&
+          !newline.test(input.read(stack.pos, input.pos - 1)) &&
+          stack.canShift(terms.noSemi))
+        input.accept(terms.noSemi, start)
     })
   } else if (name == "postfix") {
     const plus = "+".charCodeAt(0), minus = "-".charCodeAt(0)
     return new ExternalTokenizer((input, stack) => {
       let next = input.next()
       if ((next == plus || next == minus) && next == input.next() &&
-          !newline.test(input.read(stack.pos, input.pos - 2)))
+          !newline.test(input.read(stack.pos, input.pos - 2)) && stack.canShift(terms.postfixOp))
         input.accept(terms.postfixOp)
+    })
+  } else if (name == "template") {
+    const [dollar, backtick, backslash, brace] = ["$", "`", "\\", "{"].map(ch => ch.charCodeAt(0))
+    return new ExternalTokenizer((input, stack) => {
+      let start = input.pos, afterDollar = false
+      for (;;) {
+        let next = input.next()
+        if (next < 0) {
+          if (input.pos > start) input.accept(terms.templateContent, input.pos)
+          break
+        } else if (next == backtick) {
+          if (input.pos == start + 1) input.accept(terms.templateEnd)
+          else input.accept(terms.templateContent, input.pos - 1)
+          break
+        } else if (next == brace && afterDollar) {
+          if (input.pos == start + 2) input.accept(terms.templateDollarBrace)
+          else input.accept(terms.templateContent, input.pos - 2)
+          break
+        } else if (next == backslash) {
+          input.next()
+        }
+        afterDollar = next == dollar
+      }
     })
   } else {
     throw new Error("Unexpected external tokenizer name " + name)
@@ -64,7 +89,7 @@ for (let file of fs.readdirSync(caseDir)) {
       let m = caseExpr.exec(content)
       if (!m) throw new Error("Unexpected file format in " + file)
       it(m[1], () => {
-        let text = m[2].trim(), expected = compressAST(m[3])
+        let text = m[2].trim(), expected = compressAST(m[3], file)
         let strict = expected.indexOf("⚠") < 0, parser = force()
         let result = parser.parse(new StringStream(text.trim()), {strict})
         let parsed = result.toString(parser)
