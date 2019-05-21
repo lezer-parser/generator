8a119a9530bb3deaeff29d4d0b95c9ca3b46314a
Marijn Haverbeke <marijn@haverbeke.nl>
2019-05-21 11:17:34 +0200

    Update tokenizer file

---
+++ javascript/src/tokens.js
@@ -0,0 +1,67 @@
+// Hand-written tokenizers for JavaScript tokens that can't be
+// expressed by lezer's built-in tokenizer.
+
+import {insertSemi, noSemi, postfixOp, templateContent, templateDollarBrace, templateEnd} from "parser.terms.js"
+
+const newline = [10, 13, 8232, 8233]
+const space = [9, 11, 12, 32, 133, 160, 5760, 8192, 8193, 8194, 8195, 8196, 8197, 8198, 8199, 8200, 8201, 8202, 8239, 8287, 12288]
+
+const braceR = 125, braceL = 123, semicolon = 59, slash = 47, star = 42,
+      plus = 43, minus = 45, dollar = 36, backtick = 96, backslash = 92
+
+// FIXME this should technically enter block comments
+function newlineBefore(input, pos) {
+  for (let i = pos - 1; i >= 0; i--) {
+    let prev = input.peek(i)
+    if (newline.includes(prev)) return true
+    if (!space.includes(prev)) break
+  }
+  return false
+}
+
+export const insertSemicolon = new ExternalTokenizer((input, stack) => {
+  let start = input.pos, next = input.next()
+  if ((next == brace || next == -1 || newlineBefore(input, start)) && stack.canShift(insertSemi))
+    input.accept(insertSemi, start)
+})
+
+export const noSemicolon = new ExternalTokenizer((input, stack) => {
+  let start = input.pos, next = input.next()
+  if (space.includes(next) || newline.includes(next)) return
+  if (next == slash) {
+    let after = input.next()
+    if (after == slash || after == star) return
+  }
+  if (next != brace && next != semicolon && next != -1 && !newlineBefore(input, start) &&
+      stack.canShift(noSemi))
+    input.accept(noSemi, start)
+})
+
+export const postfix = new ExternalTokenizer((input, stack) => {
+  let next = input.next()
+  if ((next == plus || next == minus) && next == input.next() &&
+      !newlineBefore(input, input.pos - 2) && stack.canShift(postfixOp))
+    input.accept(postfixOp)
+})
+
+export const template = new ExternalTokenizer((input, stack) => {
+  let start = input.pos, afterDollar = false
+  for (;;) {
+    let next = input.next()
+    if (next < 0) {
+      if (input.pos > start) input.accept(templateContent, input.pos)
+      break
+    } else if (next == backtick) {
+      if (input.pos == start + 1) input.accept(templateEnd)
+      else input.accept(templateContent, input.pos - 1)
+      break
+    } else if (next == brace && afterDollar) {
+      if (input.pos == start + 2) input.accept(templateDollarBrace)
+      else input.accept(templateContent, input.pos - 2)
+      break
+    } else if (next == backslash) {
+      input.next()
+    }
+    afterDollar = next == dollar
+  }
+})
