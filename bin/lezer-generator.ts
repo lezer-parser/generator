225cf9518ee9e0733bf090c2de2093362b9f1377
Marijn Haverbeke <marijn@haverbeke.nl>
2019-07-09 20:12:57 +0200

    Add and tweak some options to the command-line program

--- bin/lezer-generator.ts
+++ bin/lezer-generator.ts
@@ -1,8 +1,10 @@
 import {buildParserFile} from ".."
 
-let file = undefined, out = undefined, moduleStyle = "CommonJS", includeNames = false
+let file = undefined, out = undefined, moduleStyle = "es", includeNames = false, exportName = undefined, noTerms = false
 
 let {writeFileSync, readFileSync} = require("fs")
+
+const usage = "Usage: build-parser [--cjs] [--names] [--noTerms] [--output outfile] [--export name] file"
 
 for (let i = 2; i < process.argv.length;) {
   let arg = process.argv[i++]
@@ -10,15 +12,19 @@ for (let i = 2; i < process.argv.length;) {
     if (file) error("Multiple input files given")
     file = arg
   } else if (arg == "--help") {
-    console.log("Usage: build-parser [--es6] [--names] file")
+    console.log(usage)
     process.exit(0)
-  } else if (arg == "--es6") {
-    moduleStyle = "es6"
+  } else if (arg == "--cjs") {
+    moduleStyle = "cjs"
   } else if (arg == "-o" || arg == "--output") {
     if (out) error("Multiple output files given")
     out = process.argv[i++]
   } else if (arg == "--names") {
     includeNames = true
+  } else if (arg == "--export") {
+    exportName = process.argv[i++]
+  } else if (arg == "--noTerms") {
+    noTerms = true
   } else {
     error("Unrecognized option " + arg)
   }
@@ -28,12 +34,13 @@ if (!file) error("No input file given")
 
 function error(msg: string) {
   console.error(msg)
+  console.log(usage)
   process.exit(1)
 }
 
 let parser, terms
 try {
-  ;({parser, terms} = buildParserFile(readFileSync(file, "utf8"), {fileName: file, moduleStyle, includeNames}))
+  ;({parser, terms} = buildParserFile(readFileSync(file, "utf8"), {fileName: file, moduleStyle, includeNames, exportName}))
 } catch (e) {
   console.error(e instanceof SyntaxError ? e.message : e.stack)
   process.exit(1)
@@ -43,8 +50,8 @@ if (out) {
   let ext = /^(.*)*?\.js$/.exec(out)
   let [parserFile, termFile] = ext ? [out, ext[1] + ".terms.js"] : [out + ".js", out + ".terms.js"]
   writeFileSync(parserFile, parser)
-  writeFileSync(termFile, terms)
-  console.log(`Wrote ${parserFile} and ${termFile}`)
+  if (!noTerms) writeFileSync(termFile, terms)
+  console.log(`Wrote ${parserFile}${noTerms ? "" : ` and ${termFile}`}`)
 } else {
   console.log(parser)
 }
